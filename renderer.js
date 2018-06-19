fdc5ddfbbcba341d9572ca9c1880cd151910e8d1
Blair <blair.j.jordan@gmail.com>
2018-06-19 23:52:44 +1000

    Added basic ledger lines and f clef.

--- renderer.js
+++ renderer.js
@@ -14,20 +14,34 @@ console.log(`devices available: ${portCount}`);
 // Get the name of a specified input port.
 input.getPortName(0);
 
+// http://www.somascape.org/midi/basic/notes.html
 const keys = [
-  { id : 60, name : 'c3' },
-  { id : 62, name : 'd3' },
-  { id : 64, name : 'e3' },
-  { id : 65, name : 'f3' },
-  { id : 67, name : 'g3' },
-  { id : 69, name : 'a3' },
-  { id : 71, name : 'b3' },
-  { id : 72, name : 'c4' },
-  { id : 73, name : 'c4s' },
-  { id : 74, name : 'd4' },
-  { id : 76, name : 'e4' },
-  { id : 77, name : 'f4' },
-  { id : 79, name : 'g4' },
+  { id : 40, name : 'e0',  sharp: false, ledger: true, bass: true  },
+  { id : 41, name : 'f0',  sharp: false, ledger: false, bass: true  },
+  { id : 43, name : 'g0',  sharp: false, ledger: false, bass: true  },
+  { id : 45, name : 'a1',  sharp: false, ledger: false, bass: true  },
+  { id : 47, name : 'b1',  sharp: false, ledger: false, bass: true  },
+  { id : 48, name : 'c1',  sharp: false, ledger: false, bass: true  },
+  { id : 50, name : 'd1',  sharp: false, ledger: false, bass: true  },
+  { id : 52, name : 'e1',  sharp: false, ledger: false, bass: true  },
+  { id : 53, name : 'f1',  sharp: false, ledger: false, bass: true  },
+  { id : 55, name : 'g1',  sharp: false, ledger: false, bass: true  },
+  { id : 57, name : 'a2',  sharp: false, ledger: false, bass: true  },
+  { id : 59, name : 'b2',  sharp: false, ledger: false, bass: true  },
+  { id : 60, name : 'c3',  sharp: false, ledger: true,  bass: false },
+  { id : 62, name : 'd3',  sharp: false, ledger: false, bass: false },
+  { id : 64, name : 'e3',  sharp: false, ledger: false, bass: false },
+  { id : 65, name : 'f3',  sharp: false, ledger: false, bass: false },
+  { id : 67, name : 'g3',  sharp: false, ledger: false, bass: false },
+  { id : 69, name : 'a3',  sharp: false, ledger: false, bass: false },
+  { id : 71, name : 'b3',  sharp: false, ledger: false, bass: false },
+  { id : 72, name : 'c4',  sharp: false, ledger: false, bass: false },
+  { id : 73, name : 'c4s', sharp: true,  ledger: false, bass: false },
+  { id : 74, name : 'd4',  sharp: false, ledger: false, bass: false },
+  { id : 76, name : 'e4',  sharp: false, ledger: false, bass: false },
+  { id : 77, name : 'f4',  sharp: false, ledger: false, bass: false },
+  { id : 79, name : 'g4',  sharp: false, ledger: false, bass: false },
+  { id : 81, name : 'a4',  sharp: false, ledger: true, bass: false },
 ];
 
 let pressedKeys = [];
@@ -46,8 +60,6 @@ input.on('message', function(deltaTime, message) {
   //console.log('m:' + message + ' d:' + deltaTime);
   console.log(message);
 
-    // http://www.somascape.org/midi/basic/notes.html
-
     const keyEventMessage = 144;
     const middleC = 60;
     
@@ -59,7 +71,7 @@ input.on('message', function(deltaTime, message) {
     
     // Key event for a defined key
     if (messageType == keyEventMessage
-     && typeof currentKey !== 'undefined') {
+      && typeof currentKey !== 'undefined') {
 
       if (velocity > 0) { 
         if ( !( currentKey.id in keys ) ) {
@@ -74,13 +86,27 @@ input.on('message', function(deltaTime, message) {
     
     pressedKeys.sort();
 
-     $('.live')
-      .empty()
-      .append('<span class="notecontainer"></span>');
+    $('.live-g').empty();
+    $('.live-f').empty();
 
     pressedKeys.forEach(k => {
-      $('.notecontainer').append(
-      `<span class="note ${findKey(k).name}">&#9833;</span>`);
+      let keyFound = findKey(k);
+
+      let live = (!keyFound.bass) ? $('.live-g') : $('.live-f');
+
+      live.append('<span class="notecontainer"></span>');
+
+      $( live ).children('.notecontainer').append(
+        `<span class="note ${keyFound.name}">&#9833;</span>`
+      );
+
+      if (keyFound.ledger)
+      {
+        $( live ).children('.notecontainer').append(
+          `<span class="ledgerline ${keyFound.name}" />`
+          );
+      }
+
     });
 });
 
